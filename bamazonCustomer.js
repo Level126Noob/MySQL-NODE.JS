a926036260a98c394f773993be45bc14ddc952c6
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-07-27 17:32:30 -0400

    Seems to be working other than creating the order and deleting the right amount from the database, still working on it.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,6 +9,7 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+function itemSearch() {}
 connection.connect(function (err) {
     if (err) {
         throw err;
@@ -16,68 +17,88 @@ connection.connect(function (err) {
 });
 
 inquirer.prompt([{
-            type: "input",
-            message: "What is the Store ID of the product would you like to purchase?",
-            name: "id"
-        },
-        {
-            type: "number",
-            message: "How many units of the product would you like to purchase?",
-            name: "amount"
-        }
-    ]).then(function (employeeResponse) {
-            {
-                function createOrder() {
-                    console.log("filling order...\n");
-                    connection.query(
-                        "DELETE FROM products WHERE ?", {
-                            item_id: employeeResponse.id
-                        },
-                        function (err, res) {
-                            if (err) {
-                                throw err;
-                            }
-                            console.log("\nYou bought an item with the product id of " + employeeResponse.id + " today. Thank You!");
-                            console.log("\nYou bought " + employeeResponse.amount + " products today, Thank You!");
-                            console.log(res.affectedRows + " products Sold!\n");
-                        }
-                    )
-                };
-
-                function checkAvaliability() {
-                    console.log("checking if product is avaliable...\n");
-                    connection.query(
-                        "SELECT stock_quantity FROM products WHERE ?", {
-                            item_id: employeeResponse.id,
-                        },
-
-                        function (err, res) {
-                            if (err) {
-                                throw err;
-                            }
-                            console.log(res);
-                            if (res < employeeResponse.amount) {
-                                console.log("Sorry, we don't have enough product for you! Check these other items in the store!")
-                            } else {
-                                createOrder();
-                            }
-                        }
-                    )
-                }
-
-                function updateInventory() {
-                    console.log("Checking all remaining Stocks...\n");
-                    connection.query(
-                        "SELECT * FROM products",
-                        function (err, res) {
-                            if (err) {
-                                throw err;
-                            }
-                            console.log(res);
-                        });
-
-                }
-                checkAvaliability();
-                updateInventory();
+    type: "input",
+    message: "What is the Store ID of the product would you like to purchase?",
+    name: "id"
+},
+{
+    type: "number",
+    message: "How many units of the product would you like to purchase?",
+    name: "amount"
+}
+]).then(function (employeeResponse) {
+    var query = "SELECT * FROM products WHERE ?"
+    connection.query(query, {
+            item_id: employeeResponse.id
+        }, function (err, res) {
+            if (err) {
+                throw err
             }
-        });
+            for (var i = 0; i < res.length; i++) {
+                console.log(
+                    "item_id: " + res[i].item_id +
+                    " || product_name: " + res[i].product_name +
+                    " || price: " + res[i].price +
+                    " || stock_quantity: " + res[i].stock_quantity
+                )
+
+                function updateProducts() {
+                    if (res[i].stock_quantity < employeeResponse.amount) {
+                        console.log("Sorry we don't have that many items in stock!")
+                    } else {
+                    var itemsLeft = res[i].stock_quantity - employeeResponse.amount
+                    console.log(itemsLeft + " Items remaining!...\n")
+                }
+            }
+                updateProducts();
+
+                function createTotal() {
+                    var price = res[i].price * employeeResponse.amount
+                    console.log("Your total is: " + price + " Dollars please!")
+                }
+                createTotal();
+                checkAvaliability();
+            }
+
+        }
+    )
+
+function deleteInventory() {
+    console.log("Deleting item from the database...\n");
+    connection.query(
+        "DELETE FROM products WHERE ?", {
+            item_id: employeeResponse.id
+        },
+        function (err, res) {
+            if (err) {
+                throw err
+            }
+            console.log(res.affectedRows + "Products deleted from database!\n");
+        }
+    )
+}
+
+
+
+function checkAvaliability() {
+    console.log("Checking if product is avaliable...\n");
+    connection.query(
+        "SELECT stock_quantity FROM products WHERE ?", {
+            item_id: employeeResponse.id,
+        },
+
+        function (err, res) {
+            if (err) {
+                throw err;
+            }
+            console.log(res);
+            if (res < employeeResponse.amount) {
+                console.log("Sorry, we don't have enough product for you!")
+                deleteInventory();
+            }
+        }
+    )
+}
+
+checkAvaliability();
+});
